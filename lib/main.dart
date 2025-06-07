a2d8e0216b29609e35a22bf3b679a3849f7219ae
kamucomecats <yuta223125@gmail.com>
2025-06-07 23:02:34 +0900

    FIX:mini ver shuffle

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,6 @@ class MyAppState extends ChangeNotifier {
     options = quiz.getNextOptions();
     quiz.increment();
     notifyListeners();
-    print(mondai);
   }
 }
 
@@ -56,14 +55,23 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('A random AAAAAAAAA idea:'),
             Question(appState: appState),
-            ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                  print('button pressed!');
-                },
-                child: Text('next'))
+            Option(
+              appState: appState,
+              index: 0,
+            ),
+            Option(
+              appState: appState,
+              index: 1,
+            ),
+            Option(
+              appState: appState,
+              index: 2,
+            ),
+            Option(
+              appState: appState,
+              index: 3,
+            ),
           ],
         ),
       ),
@@ -86,14 +94,54 @@ class Question extends StatelessWidget {
       color: theme.colorScheme.onPrimary,
     );
 
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(
-          appState.mondai,
-          style: style,
+    return SizedBox(
+      width: 300,
+      height: 300,
+      child: Card(
+        color: theme.colorScheme.primary,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: FittedBox(
+            child: Text(
+              appState.mondai,
+              style: style,
+            ),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class Option extends StatelessWidget {
+  const Option({
+    super.key,
+    required this.appState,
+    required this.index,
+  });
+
+  final MyAppState appState;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displaySmall!.copyWith();
+
+    return SizedBox(
+      width: 300,
+      height: 100,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: FittedBox(
+                child: Text(
+              appState.options[index],
+              style: style,
+            ))),
       ),
     );
   }
@@ -112,15 +160,19 @@ class Quiz {
   }
 
   final Map<String, String> quiz = {
-    '俺が今食べたいものは': '唐揚げ',
-    '夏の旬といえば': 'スイカ',
-    'あなたの好物': 'コロッケ',
-    '寒いときにおいしい': 'おでん',
-    '家のは格別な': 'ホイル焼き',
-    'たまに食べたい': 'ちまき',
-    '食べ物の王様': 'カレー',
-    '週3で食べたい': 'ホットドッグ',
+    '私は学校に走ります。': 'Ich laufe zur Schule.',
+    '私の趣味は女装です。': 'Mein Hobby ist Cross-Dressing.',
+    '私は東京出身です。': 'Ich komme aus Tokio.',
+    '私の職業は料理人です。': 'Mein Beruf ist Koch.',
+    '私の好きな科目は数学です。': 'Mein Lieblingsfach ist Mathematik.',
+    '私は図書館にいます。': 'Ich bin in der Bibliothek.',
+    '私は財布を探しています。': 'Ich suche meine Brieftasche.',
+    '私は電車を待っています。': 'Ich warte auf einen Zug.',
+    '私は彼に怒っています。': 'Ich bin wütend auf ihn.',
+    '私は風邪をひいています。': 'Ich habe eine Erkältung.',
   };
+
+  List<List<int>> history = [];
 
   List keys = [];
   List values = [];
@@ -161,7 +213,7 @@ class Quiz {
   //randKeys and randOptions update
   void update() {
     //randKeyUpdate
-    var randKeysIndex = List.generate(quiz.length, (i) => i).toList();
+    List<int> randKeysIndex = List.generate(quiz.length, (i) => i).toList();
     randKeysIndex.shuffle();
     randKeysIndex = randKeysIndex.take(size).toList();
     randKeys = [];
@@ -170,11 +222,17 @@ class Quiz {
     }
 
     //randOptionsUpdate
-    var randValuesIndex = [];
+    List<List<int>> randValuesIndex = [];
     for (int i = 0; i < size; i++) {
       var candidates = List.generate(quiz.length, (i) => i).toList()
         ..remove(randKeysIndex[i]);
+      candidates.shuffle();
+
       randValuesIndex.add(candidates.take(optionsNum - 1).toList());
+      randValuesIndex[i].add(randKeysIndex[i]);
+      print(randValuesIndex[i]);
+      randValuesIndex[i].shuffle();
+      print(randValuesIndex[i]);
     }
     randValues = [];
     for (int i = 0; i < size; i++) {
